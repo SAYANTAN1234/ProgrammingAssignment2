369e11a484d7fec758089779b8b1b54d8786195f
SAYANTAN1234 <sayantan9223@gmail.com>
2014-12-21 12:33:58 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -39,3 +39,26 @@ iM <- solve(data, ...)
 xm$setInvM(iM)
 iM
 }
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get, setmean = setmean, getmean = getmean)
+}
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if (!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
